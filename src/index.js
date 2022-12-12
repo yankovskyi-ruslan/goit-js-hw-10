8e8d1877555cd9f21b626c96c15ff126548eb387
yankovskyi-ruslan <tital3108@gmail.com>
2022-12-12 16:54:53 +0200

    виконує ДЗ

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const inputEl = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+const DEBOUNCE_DELAY = 300;
+
+inputEl.addEventListener(
+  'input',
+  debounce(e => {
+    const trimmedValue = inputEl.value.trim();
+    cleanHtml();
+    if (trimmedValue !== '') {
+      fetchCountries(trimmedValue).then(foundData => {
+        if (foundData.length > 10) {
+          Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+        } else if (foundData.length === 0) {
+          Notify.failure('Oops, there is no country with that name');
+        } else if (foundData.length >= 2 && foundData.length <= 10) {
+          renderCountryList(foundData);
+        } else if (foundData.length === 1) {
+          renderOneCountry(foundData);
+        }
+      });
+    }
+  }, DEBOUNCE_DELAY)
+);
+
+function renderCountryList(countries) {
+  const markup = countries
+    .map(country => {
+      return `<li>
+      <img src="${country.flags.svg}" alt="Flag of ${country.name.official}" width="30" hight="20">
+         <b>${country.name.official}</p>
+                </li>`;
+    })
+    .join('');
+  countryList.innerHTML = markup;
+}
+
+function renderOneCountry(countries) {
+  const markup = countries
+    .map(country => {
+      return `<li>
+      <img src="${country.flags.svg}" alt="Flag of ${
+        country.name.official
+      }" width="30" hight="20">
+         <h2>${country.name.official}</h2></p>
+            <p><b>Capital</b>: ${country.capital}</p>
+            <p><b>Population</b>: ${country.population}</p>
+            <p><b>Languages</b>: ${Object.values(country.languages)} </p>
+                </li>`;
+    })
+    .join('');
+  countryList.innerHTML = markup;
+}
+
+function cleanHtml() {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+}
